c6c0c07ef109bf1fff1b827e2816487801c4b959
lucasverteiro <verteirosantos@gmail.com>
2025-06-26 15:27:58 -0300

    mudanças

--- js/logica.js
+++ js/logica.js
@@ -1,1 +1,6 @@
-document.getElementById
+var end = window.document.getElementById('endereço')
+eml.addEventListener('click', clicar)
+
+function clicar(){
+    var eml = window.document.getElementById('email')
+}
