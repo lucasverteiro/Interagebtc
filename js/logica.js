e4b3d5886abe3d19bdb34c47e444b0b8946197bd
lucasverteiro <verteirosantos@gmail.com>
2025-06-27 14:15:05 -0300

    Update logica.js

--- js/logica.js
+++ js/logica.js
@@ -1,9 +1,10 @@
-var end = window.document.getElementById('endereço')
-eml.addEventListener('click', clicar)
 
 function clicar(){
     var eml = window.document.getElementById('email')
     if (eml) {
-        window.open("https://lucasverteiro.github.io/Interagebtc", "_blank")
+        console.open("https://lucasverteiro.github.io/Interagebtc")
     }
 }
+
+var end = window.document.getElementById('endereço')
+eml.addEventListener('click', clicar)
