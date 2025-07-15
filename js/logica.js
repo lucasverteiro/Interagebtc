0e57b843bc664854b3ebec8d72b1cb442e8f8444
lucasverteiro <verteirosantos@gmail.com>
2025-07-15 20:24:29 -0300

    modificações

--- js/logica.js
+++ js/logica.js
@@ -1,8 +1,24 @@
 
+let segundos = 15;
+const temporizadorElemento = document.getElementById('temporizador')
+function atualizarTemporizador() {
+    temporizadorElemento.textContent = segundos;
+    segundos--;
+    if (segundos < 0) {
+        window.history.back();
+    } else {
+        setTimeout(atualizarTemporizador,1000)
+    }
+}
+
+atualizarTemporizador();
+
 function clicar(){
     var eml = window.document.getElementById('email')
     if (eml) {
         console.open("https://lucasverteiro.github.io/Interagebtc")
+    } else {
+        
     }
 }
 
