19c9e2e651fbe893cee3e2501ec85488afafa3ba
lucasverteiro <verteirosantos@gmail.com>
2025-09-18 23:27:18 -0300

    Update logica.js

--- logica.js
+++ logica.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const botaoAcoes = document.getElementById('continuar');
     botaoAcoes.addEventListener('click', function() {
+        const enderecoBitcoin = document.getElementById('enderecos').value;
+        const valorEmBitcoin = '0.00000001';
+        if (enderecoBitcoin) {
+            const uriBitcoin = `bitcoin:${endereçodepagamento}?amount=${0.00000001}`;
+            window.location.href = uriBitcoin;
+
+        } else {
+            alert('Por favor, digite um endereço Bitcoin válido.');
+        }
         window.open('https://www.youtube.com/', '_blank');
-        window.location.href = '../html/respaw.html';
+        window.location.href = 'html/respaw.html';
     });
 });
